c3411f44d2785a93e7ae3362cab5c06d0a966216
Raj Shukla <rajshukla9630@gmail.com>
2026-05-14 14:23:27 +0530

    L63 adding more state

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ const messages = [
 export default function App(){
 
   const [step,setStep] = useState(1);
+  const [isOpen, setIsOpen]= useState(true);
+  const [buttonName,setButtonName] = useState('Hide');
 
   function handlePrevious(){
       if(step <= 1)return;
@@ -21,24 +23,40 @@ export default function App(){
     setStep(step=> step + 1);
   }
 
-  return <div className="steps">
+  function handleShowHide(){
+    if(isOpen){
+      setIsOpen(false);
+      setButtonName('Show');
+    }
+    else{
+      setIsOpen(true);
+      setButtonName('Hide');
+    }
+  }
 
-        <div className="numbers">
-          <div className={step >= 1 ? 'active':''}>1</div>
-          <div className={step >= 2 ? 'active':''}>2</div>
-          <div className={step == 3 ? 'active':''}>3</div>
-        </div>
+  return (
+  <div>
+    <button onClick={handleShowHide}>{buttonName}</button>
+    { isOpen ? 
+    <div className="steps">
 
-        <p className="message">Step {step} : {messages[step-1]}</p>
+          <div className="numbers">
+            <div className={step >= 1 ? 'active':''}>1</div>
+            <div className={step >= 2 ? 'active':''}>2</div>
+            <div className={step == 3 ? 'active':''}>3</div>
+          </div>
 
-        <div className="buttons">
-          <button style={{backgroundColor: '#7950f2', color: '#fff'}}
-          onClick={handlePrevious}
-          >Previous</button>
-          <button style={{backgroundColor:'#7950f2' , color:'#fff'}}
-          onClick={handleNext}
-          >Next</button>
-        </div>
-    
-      </div>
+          <p className="message">Step {step} : {messages[step-1]}</p>
+
+          <div className="buttons">
+            <button style={{backgroundColor: '#7950f2', color: '#fff'}}
+            onClick={handlePrevious}
+            >Previous</button>
+            <button style={{backgroundColor:'#7950f2' , color:'#fff'}}
+            onClick={handleNext}
+            >Next</button>
+          </div>
+      
+    </div> : <p>UI Hidden</p>}
+  </div>)
 }
