6989ce7bb60c62374e0caedd9d6dc603db35d50f
Raj Shukla <rajshukla9630@gmail.com>
2026-05-14 11:54:09 +0530

    L58-60 handling state by useState

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -6,7 +8,18 @@ const messages = [
 
 export default function App(){
 
-  const step = 2;
+  const [step,setStep] = useState(1);
+
+  function handlePrevious(){
+      if(step <= 1)return;
+      setStep((step)=> step - 1);
+  }
+
+
+  function handleNext(){
+    if(step >= 3) return;
+    setStep(step=> step + 1);
+  }
 
   return <div className="steps">
 
@@ -19,8 +32,12 @@ export default function App(){
         <p className="message">Step {step} : {messages[step-1]}</p>
 
         <div className="buttons">
-          <button style={{backgroundColor: '#7950f2', color: '#fff'}}>Previous</button>
-          <button style={{backgroundColor:'#7950f2' , color:'#fff'}}>Next</button>
+          <button style={{backgroundColor: '#7950f2', color: '#fff'}}
+          onClick={handlePrevious}
+          >Previous</button>
+          <button style={{backgroundColor:'#7950f2' , color:'#fff'}}
+          onClick={handleNext}
+          >Next</button>
         </div>
     
       </div>
